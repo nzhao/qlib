008f3232b81e064f78c51aafa31b14f536db6381
nzhao <nzhao@csrc.ac.cn>
2015-05-05 19:16:07 +0800

    SpinInteraction

---
+++ +model/+phy/+SpinInteraction/@DipolarInteraction/DipolarInteraction.m
@@ -0,0 +1,12 @@
+classdef DipolarInteraction
+    %DIPOLARINTERACTION Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ +model/+phy/+SpinInteraction/@HyperfineInteraction/HyperfineInteraction.m
@@ -0,0 +1,12 @@
+classdef HyperfineInteraction
+    %HYPERFINEINTERACTION Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
