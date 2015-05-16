fc7d3d6d00dfd7c15a4b974aecc617df9f889780
nzhao <nzhao@csrc.ac.cn>
2015-05-16 20:28:39 +0800

    Test BenzeneProton Evolution

--- temp/dynamics_example2.m
+++ temp/dynamics_example2.m
@@ -19,7 +19,7 @@ import model.phy.SpinInteraction.SpinOrder
 import model.phy.SpinInteraction.GeneralSpinInteraction
 
 %strategies
-import model.phy.SpinCollection.Strategy.FromSpinList
+import model.phy.SpinCollection.Strategy.FromFile
 import model.phy.QuantumOperator.MatrixStrategy.FromProductSpace
 import model.phy.QuantumOperator.MatrixStrategy.FromHamiltonianToLiouvillian
 import model.phy.Dynamics.EvolutionKernel.MatrixVectorEvolution
@@ -27,15 +27,14 @@ import model.phy.Dynamics.EvolutionKernel.MatrixVectorEvolution
 %% FromSpinList 
 
 spin_collection=SpinCollection();
-spin_collection.spin_source = FromSpinList(...
-    [Spin('13C', [0 0 1.54]),...
-     Spin('13C', [0 0 0])]...
+spin_collection.spin_source = FromFile(...
+    [INPUT_FILE_PATH, '+xyz/BenzeneProton.xyz']...
      );
 spin_collection.generate();
 
 %% SpinInteraction
 
-para.B=1e-4*[0 0 1000.];
+para.B=1e-4*[0 0 100];
 hami=Hamiltonian(spin_collection);
 liou=Liouvillian(spin_collection);
 liou.matrix_strategy=FromHamiltonianToLiouvillian(hami);
@@ -48,7 +47,8 @@ liou.generate_matrix();
 
 denseMat=DensityMatrix(spin_collection);
 %denseMat.addSpinOrder( SpinOrder(spin_collection, {[1,2]}, { {'sz', 'sz'} }) );
-denseMat.addSpinOrder( GeneralSpinInteraction(spin_collection, {[1,2],}, { {[1 0; 0 0], [0 0; 0 1]}, }, {1.0,}) );
+%denseMat.addSpinOrder( GeneralSpinInteraction(spin_collection, {[1,2],}, { {[1 0; 0 0], [0.5 0; 0 0.5]}, }, {1.0,}) );
+denseMat.addSpinOrder( GeneralSpinInteraction(spin_collection, {1,}, { {[1 0; 0 0]}, }, {1.0,}) );
 denseMat.generate_matrix();
 
 %% Observable
@@ -68,7 +68,7 @@ obs2.generate_matrix();
 
 dynamics=QuantumDynamics( MatrixVectorEvolution(liou) );
 dynamics.set_initial_state(denseMat);
-dynamics.set_time_sequence(0:2e-6:10e-4);
+dynamics.set_time_sequence(0:20e-6:5e-3);
 dynamics.addObervable(obs1);
 dynamics.addObervable(obs2);
 dynamics.calculate_mean_values();
