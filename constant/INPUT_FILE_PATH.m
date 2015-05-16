2c188edfd02c3b06e91afe98e9920700e8e622cf
nzhao <nzhao@csrc.ac.cn>
2015-05-16 20:17:31 +0800

    Test BenzeneProton evolution.

--- constant/INPUT_FILE_PATH.m
+++ constant/INPUT_FILE_PATH.m
@@ -2,6 +2,6 @@ function path = INPUT_FILE_PATH(  )
 %INPUT_FILE_PATH Summary of this function goes here
 %   Detailed explanation goes here
 path='/Users/nzhao/code/lib/active/qlib/+controller/+input/';
-
+path='C:\Users\Nan\Documents\GitHub\qlib\+controller\+input\';
 end
 
