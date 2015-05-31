62e8cc86731e0a5985ecabd65ef2e6d075e26abd
nzhao <nzhao@csrc.ac.cn>
2015-05-31 21:20:30 +0800

    test rotating frame... not finished.

--- temp/dynamics_example2.m
+++ temp/dynamics_example2.m
@@ -11,6 +11,7 @@ import model.phy.Dynamics.QuantumDynamics
 
 %interactoins
 import model.phy.SpinInteraction.ZeemanInteraction
+import model.phy.SpinInteraction.RotatingFrameInteraction
 import model.phy.SpinInteraction.DipolarInteractionSecular
 
 %strategies
@@ -21,24 +22,26 @@ import model.phy.Dynamics.EvolutionKernel.MatrixVectorEvolution
 
 spin_collection=SpinCollection();
 spin_collection.spin_source = FromFile(...
-    [INPUT_FILE_PATH, '+xyz/butaneProton.xyz']...
+    [INPUT_FILE_PATH, '+xyz/BenzeneProtonZPlane.xyz']...
      );
 spin_collection.generate();
 
 %% SpinInteraction
-
+Condition=LabCondition.getCondition;
+Condition.setValue('magnetic_field', 1e-4*100*[1 1 1]);
 para.B=1e-4*[0 0 100];
-para.rotation_frequency=para.B*spin_collection.spin_list{1}.gamma;
+% para.rotation_frequency=para.B*spin_collection.spin_list{1}.gamma;
 
 hami=Hamiltonian(spin_collection);
-hami.addInteraction( ZeemanInteraction(spin_collection, para) );
-hami.addInteraction( DipolarInteractionSecular(spin_collection, para) );
+hami.addInteraction( ZeemanInteraction(spin_collection) );
+% hami.addInteraction( RotatingFrameInteraction(spin_collection, para.B(3)*spin_collection.spin_list{1}.gamma) );
+hami.addInteraction( DipolarInteractionSecular(spin_collection) );
 
 liou=hami.circleC();
-
+disp('liou generated');
 %% DensityMatrix
 
-denseMat=DensityMatrix(spin_collection, {'1.0 * p(2)_1 * p(1)_2 * p(1)_3 * p(1)_4 * p(1)_5 * p(1)_6 * p(1)_7 * p(1)_8 * p(1)_9 * p(1)_10'});
+denseMat=DensityMatrix(spin_collection, {'1.0 * p(2)_1 * p(1)_2 * p(1)_3 * p(1)_4 * p(1)_5 * p(1)_6'});
 
 %% Observable
 
