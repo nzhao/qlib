4f173daf2178263b26fa2980595a4ecae550df1e
Li-Ping Yang <yanglp091@163.com>
2015-07-24 16:18:23 +0800

    add a evolution strategy
    1. Generate evolution matrix step by step in
    “DensityMatrixEvolution.m”, so that the evolution step costs much less
    memory
    
    2. Add a new evolution strategy “ObservableMatrixEvolution.m”. During
    the evolution, the  matlab will not save the density matrix, the the
    evolution step costs even less memory. But this strategy will just give
    the values of the observables not the density matrix.

--- +model/+phy/+Dynamics/+EvolutionKernel/DensityMatrixEvolution.m
+++ +model/+phy/+Dynamics/+EvolutionKernel/DensityMatrixEvolution.m
@@ -1,6 +1,7 @@
 classdef DensityMatrixEvolution< model.phy.Dynamics.AbstractEvolutionKernel
     %ENSEMBLECCE 
-    %This class is used to calculate the time evolutuion of the coherence for ensemble CCE method 
+    %This class is used to calculate the time evolutuion of the density
+    %matrix in the Hilbert Spase
     
     properties
         timelist
@@ -19,35 +20,40 @@ classdef DensityMatrixEvolution< model.phy.Dynamics.AbstractEvolutionKernel
                end
                
                obj.matrixList=cell(1,noperator);
+
                for n=1:noperator
                    obj.matrixList{1,n}=obj.matrix_prefactor(n)*qoperatorList{n}.getMatrix();
                end
                 obj.result=0;
           end
-          
+                 
           function state_out = calculate_evolution(obj, state_in, timelist)
-                obj.timelist=timelist;
-                ntime=length(timelist);
-                dt=timelist(2)-timelist(1);
+               obj.timelist=timelist;
+               ntime=length(timelist);
+               dt=timelist(2)-timelist(1);
 
-                noperator=length(obj.matrixList);
-                coh_mat_list=cell(1,noperator);   
-                for m=1:noperator %initial the core matrice
-                     core=1i*dt*obj.matrixList{1,m};
-                     coh_mat_list{m}=model.math.linear_sequence_expm(core,ntime-1,1);
-                end
-                
+               noperator=length(obj.matrixList);
+               core_mat_list=cell(1,noperator);
+               evolution_mat_list=cell(1,noperator);
+               for m=1:noperator %initial the core matrice                  
+                   core_mat_list{m}=expm(1i*dt*obj.matrixList{1,m});
+                   evolution_mat_list{m}=1;
+               end
                state_out=cell(1,ntime);
-               for n=1:ntime
-                   mat=1;
-                   for m=1:noperator
-                       if m==noperator/2
-                           mat=mat*coh_mat_list{m}{n}*state_in;
-                       else
-                           mat=mat*coh_mat_list{m}{n};
-                       end
+               state_out{1,1}=state_in;
+               for n=2:ntime                   
+                   for m=1:noperator                      
+                     evolution_mat_list{1,m}=evolution_mat_list{1,m}*core_mat_list{1,m};
                    end
-                   state_out{1,n}=mat;
+                   mat=1;
+                  for m=1:noperator
+                   if m==noperator/2
+                       mat=mat*evolution_mat_list{1,m}*state_in;
+                   else
+                       mat=mat*evolution_mat_list{1,m};
+                   end
+                  end
+                  state_out{1,n}=mat;
                end
                 obj.result=state_out;
           end
