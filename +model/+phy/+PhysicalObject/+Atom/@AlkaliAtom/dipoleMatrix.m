c4a81ea53af37b9ce5f40c0edb2da5a893dedd0d
zhangke2821 <zkwzk@csrc.ac.cn>
2015-10-12 18:35:30 +0800

    Revert "changes in dipoleMatrix"
    This reverts commit 0dc52572a77e37f2ff908eea7abd2b10d8a4f908.

--- +model/+phy/+PhysicalObject/+Atom/@AlkaliAtom/dipoleMatrix.m
+++ +model/+phy/+PhysicalObject/+Atom/@AlkaliAtom/dipoleMatrix.m
@@ -1,10 +1,6 @@
 function Djss = dipoleMatrix( obj, J, magB )
 %DIPOLEMATRIX Summary of this function goes here
 %   Detailed explanation goes here
-    S=obj.parameters.spin_S;
-    I=obj.parameters.spin_I;
-    gI=2*I+1; gS=2*S+1; gJ=2*J+1;%gg=gI*gS; ge=gI*gJ;
-    
     [~,Ug,~,Ue]=obj.eigenValueVector(J,magB);
     sDs=zeros(gJ,gS,3); %spherical projections in electronic space
     for k=J:-1:-J
