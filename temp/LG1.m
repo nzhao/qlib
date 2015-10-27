43a887cd152f4c014344e34e4902fae6c561a30e
Lei-Ming Zhou <lm.zhou2@gmail.com>
2015-10-27 16:35:23 +0800

    test1

--- temp/LG1.m
+++ temp/LG1.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 clear all; clc
 
 wavelength=1.;intensity=1.0;f0=1.0; na=0.8; n1=1.0; n2=1.0; px=1.0; py=0.0;p=0; l=-3;
@@ -6,9 +5,8 @@ lg1=model.phy.PhysicalObject.LaserBeam.AplanaticBeam.LinearCircularPol(wavelengt
 lg1.getVSWFcoeff(40);
 figure;dataF1=lg1.lineCut([-1, 0, 0], [1, 0, 0], 100, 'field');
 figure;dataD1=lg1.lineCut([-1, 0, 0], [1, 0, 0], 100);
-=======
-clear; clc
 
+clear; clc
 import model.phy.PhysicalObject.Lens
 import model.phy.PhysicalObject.LaserBeam.ParaxialBeam.ParaxialLaguerreGaussianBeam
 
@@ -27,6 +25,3 @@ lg2.getVSWFcoeff(80);
 [val1e, val1h]=lg2.wavefunction(1, 0, 0.1);
 [val2e, val2h]=lg2.focBeam.wavefunction(1, 0, 0.1);
 
-
-
->>>>>>> refs/remotes/nzhao/master
