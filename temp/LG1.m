b4fd16b31519ea2648d00c8f0825438649128805
nzhao <nzhao@csrc.ac.cn>
2015-10-29 14:37:09 +0800

    Revert "My testzhou.m added and conflit solved."
    This reverts commit 9b616ee605378ebc3515affd8136fe5ed5f9a8e6.

--- temp/LG1.m
+++ temp/LG1.m
@@ -1,5 +1,12 @@
-clear; clc;
+clear all; clc
 
+wavelength=1.;intensity=1.0;f0=1.0; na=0.8; n1=1.0; n2=1.0; px=1.0; py=0.0;p=0; l=-3;
+lg1=model.phy.PhysicalObject.LaserBeam.AplanaticBeam.LinearCircularPol(wavelength, intensity, f0, na, n1, n2, px, py, p,l);
+lg1.getVSWFcoeff(40);
+figure;dataF1=lg1.lineCut([-1, 0, 0], [1, 0, 0], 100, 'field');
+figure;dataD1=lg1.lineCut([-1, 0, 0], [1, 0, 0], 100);
+
+clear; clc
 import model.phy.PhysicalObject.Lens
 import model.phy.PhysicalObject.LaserBeam.ParaxialBeam.ParaxialLaguerreGaussianBeam
 
@@ -9,22 +16,12 @@ lens=Lens(f, NA, working_medium);
 
 power=1.0; 
 wavelength=0.5; waist=1000.0; center=[0, 0, 0];  %in micron
-px=1.0; py=0.0; p=0; l=1; 
-incBeam1=ParaxialLaguerreGaussianBeam(wavelength, power, waist, center, p, l, px, py, 'vacuum');
-incBeam2=ParaxialLaguerreGaussianBeam(wavelength, power, waist, center, p, -l, px, py, 'vacuum');
+px=1.0; py=0.0; p=0; l=-1; 
+incBeam=ParaxialLaguerreGaussianBeam(wavelength, power, waist, center, p, l, px, py, 'vacuum');
 
-lg1=model.phy.PhysicalObject.LaserBeam.AplanaticBeam.LinearCircularPol(lens, incBeam1);
-lg2=model.phy.PhysicalObject.LaserBeam.AplanaticBeam.LinearCircularPol(lens, incBeam2);
-
-lg1.getVSWFcoeff(80);
+lg2=model.phy.PhysicalObject.LaserBeam.AplanaticBeam.LinearCircularPol(lens, incBeam);
 lg2.getVSWFcoeff(80);
 
-x=1.0; y=0.1; z=0.1;
-[e1plus, h1plus]=lg1.wavefunction(x, y, z);
-[e2plus, h2plus]=lg1.focBeam.wavefunction(x, y, z);
+[val1e, val1h]=lg2.wavefunction(1, 0, 0.1);
+[val2e, val2h]=lg2.focBeam.wavefunction(1, 0, 0.1);
 
-[e1minus, h1minus]=lg2.wavefunction(x, y, z);
-[e2minus, h2minus]=lg2.focBeam.wavefunction(x, y, z);
-
-[e1minus; e1plus]
-[e2minus; e2minus]
