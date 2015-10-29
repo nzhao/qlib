1a27c45185d19361f199707a350d98a737dfa7a0
nzhao <nzhao@csrc.ac.cn>
2015-10-29 14:48:02 +0800

    resolve confilit.

--- temp/LG1bak.m
+++ temp/LG1bak.m
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-clear all; clc
-
-wavelength=1.;intensity=1.0;f0=1.0; na=0.8; n1=1.0; n2=1.0; px=1.0; py=0.0;p=0; l=-3;
-lg1=model.phy.PhysicalObject.LaserBeam.AplanaticBeam.LinearCircularPol(wavelength, intensity, f0, na, n1, n2, px, py, p,l);
-lg1.getVSWFcoeff(40);
-figure;dataF1=lg1.lineCut([-1, 0, 0], [1, 0, 0], 100, 'field');
-figure;dataD1=lg1.lineCut([-1, 0, 0], [1, 0, 0], 100);
-=======
 clear; clc
 
 import model.phy.PhysicalObject.Lens
@@ -27,6 +18,3 @@ lg2.getVSWFcoeff(80);
 [val1e, val1h]=lg2.wavefunction(1, 0, 0.1);
 [val2e, val2h]=lg2.focBeam.wavefunction(1, 0, 0.1);
 
-
-
->>>>>>> refs/remotes/nzhao/master
