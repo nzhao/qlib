39b82e6e38eff6158455e860ed32127f169f2942
Li-Ping Yang <yanglp091@163.com>
2015-11-02 22:05:55 +0800

    add a subclass of SpinOperator---DecoherenceSuperOperator and a Lab Condition---temperature
    I add a superoperator in the Liouville space to describe the
    decoherence of spin-half system phenomenologically.
    
    I add the temperature as a new LabCondition parameter

---
+++ +model/+phy/+QuantumOperator/+SpinOperator/@DecoherenceSuperOperator/DecoherenceSuperOperator.m
@@ -0,0 +1,36 @@
+classdef DecoherenceSuperOperator < model.phy.QuantumOperator.MultiSpinSuperOperator
+    %DECOHERENCESUPEROPERATOR generate a superoperator to describe the decoherence of the multi-spin system.
+    % the total decoherence operator in the Liouville space is the summation of the decoherence operator 
+    % for each single spin. But if the spin is not spin half, the corresponding decoherence operator is 
+    % replaced by the zero matrix. 
+
+    %   The decoherence operator of the single spin is of the Lindblad form:
+    %  L rho = (1/2)*Gamma_vertical*[f(omega)+1](2sigma_{-}*rho*sigma_{+} - rho*sigma_{+}sigma_{-} - sigma_{+}sigma_{-}*rho)
+    %          +(1/2)*Gamma_vertical*f(omega)*(2sigma_{+}*rho*sigma_{-} - rho*sigma_{-}sigma_{+} - sigma_{-}sigma_{+}*rho)
+    %          -Gamma_parallel*sigma_{+}*rho*sigma_{+} - Gamma_parallel*sigma_{-}*rho*sigma_{-}
+    % where sigma is the Pauli operator of the spin, Gamma_vertical (Gamma_vertical) describe the vertical (parallel) coupling 
+    % between the spin and its bath, omega is the Zeeman splitting of the spin, and f(omega)=1/[exp(hbar*omega/kT)] is the 
+    % occupation number of the bosonic mode with frequency omega.
+    
+    
+    properties
+        decay_rate_list
+        
+    end
+    
+    methods
+        function obj=DecoherenceSuperOperator(spin_collection,decay_rate_list)
+            obj@model.phy.QuantumOperator.MultiSpinSuperOperator(spin_collection);
+            obj.name='decoherence_super_operator';
+            
+            obj.decay_rate_list=decay_rate_list;
+        end
+        
+        function generate_matrix(obj)
+            obj.matrix=obj.calculate_matrix();
+            obj.hasMatrix=1;
+        end
+    end
+    
+end
+
