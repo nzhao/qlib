4a4b5d2a2ca260cee0d6aef3aa359e76d1223772
Lei-Ming Zhou <lm.zhou2@gmail.com>
2015-11-22 20:29:02 +0800

    for sync

--- temp/Dtest.m
+++ temp/Dtest.m
@@ -6,7 +6,7 @@ krP=0.5; thetaP=0;phiP=0;
 MP1 = spharm(n,m,thetaP,phiP)*(-1);MP1
 R = ott13.z_rotation_matrix(pi/3,0);%R = rotation_matrix([0,1,0],pi/3); %The same as zeng, rotate sate.
 D = ott13.wigner_rotation_matrix(Nmax,R);D=full(D);
-D1=D'% This is the D of zeng, verified.
+D1=D';% This is the D of zeng, verified.
 % add m'*alpha to get D2
 alpha=phiL;
 Dzeng=[D1(1,:)*exp(-1i*(-1)*alpha);D1(2,:)*exp(-1i*0*alpha);D1(3,:)*exp(-1i*1*alpha)];
@@ -14,7 +14,7 @@ D=full(Dzeng)
 ML1 = spharm(n,1,thetaL,phiL)*(-1);
 ML0 = spharm(n,0,thetaL,phiL);
 ML_1 = spharm(n,-1,thetaL,phiL)*(-1)^(-1);
-[ML_1;ML0;ML1]
+[ML_1;ML0;ML1];
 % Mnew=D(1,1)*ML_1+D(1,2)*ML0+D(1,3)*ML1;[MP1;Mnew]
 Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew]
 
@@ -42,12 +42,26 @@ Mnew=R*(Mnew.');Mnew=Mnew.';
 [MP1;Mnew]
 
 %% test the D
-Nmax=1;
-R = ott13.z_rotation_matrix(pi/3,0);%R = rotation_matrix([0,1,0],pi/3); %The same as zeng, rotate the sate.
-D = ott13.wigner_rotation_matrix(Nmax,R);D=full(D)
+clc;
+n=1;m=1;Nmax=1;
+krL=0.5; thetaL=pi/3;phiL=pi/6;
+krP=0.5; thetaP=0;phiP=0;
+R = ott13.z_rotation_matrix(thetaL,phiL);%R = rotation_matrix([0,1,0],pi/3); %The same as zeng, rotate the sate.
+D = ott13.wigner_rotation_matrix(Nmax,R);D=full(D.')% This is the Zeng's D
+MP1 = spharm(n,m,thetaP,phiP)*(-1);MP1
+ML1 = spharm(n,1,thetaL,phiL)*(-1);
+ML0 = spharm(n,0,thetaL,phiL);
+ML_1 = spharm(n,-1,thetaL,phiL)*(-1)^(-1);
+[ML_1;ML0;ML1];
+% Mnew=D(1,1)*ML_1+D(1,2)*ML0+D(1,3)*ML1;[MP1;Mnew]
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew]
+
+%%
 [X,Y]=meshgrid([-1,0,1]);
 mt=(-1).^(Y-X);
 D1=D./mt% This is the D of zeng
 
-R = ott13.z_rotation_matrix(-pi/3,0);
-D = ott13.wigner_rotation_matrix(Nmax,R);D=full(D.')
+m=[-1,0,1];%m=1;
+MP1 = spharm(n,m,thetaL,phiL).*(-1).^m;
+D3=sqrt(4*pi/(2*n+1))*conj(MP1);D3
+D1(:,2)
