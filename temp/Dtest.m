8badb83ce7afd85709256f7a9111e1339210c2a4
Lei-Ming Zhou <lm.zhou2@gmail.com>
2015-11-27 20:28:10 +0800

    for sync

--- temp/Dtest.m
+++ temp/Dtest.m
@@ -1,29 +1,3 @@
-%% test Ylm with the D from OTT:OK
-clc;
-n=1;m=1;Nmax=1;
-krL=0.5; thetaL=pi/3;phiL=pi/6;
-krP=0.5; thetaP=0;phiP=0;
-R = ott13.z_rotation_matrix(thetaL,phiL);
-D = ott13.wigner_rotation_matrix(Nmax,R);%D=full(D.')% This is the Zeng's D, m={-1,0,1} order.
-%test Ylm
-MP1 = spharm(n,m,thetaP,phiP)*(-1);MP1;
-ML1 = spharm(n,1,thetaL,phiL)*(-1);
-ML0 = spharm(n,0,thetaL,phiL);
-ML_1 = spharm(n,-1,thetaL,phiL)*(-1)^(-1);
-[ML_1;ML0;ML1]; %verified by mathecmatica
-Mnew=D(1,1)*ML_1+D(2,1)*ML0+D(3,1)*ML1;[MP1;Mnew]
-%test vector M
-[MP1,NP1] = vswfcart(n,m,krP,thetaP,phiP);MP1=MP1*(-1);
-[ML1,NL1] = vswfcart(n,1,krL,thetaL,phiL);ML1=ML1*(-1);
-[ML0,NL0] = vswfcart(n,0,krL,thetaL,phiL);
-[ML_1,NL_1] = vswfcart(n,-1,krL,thetaL,phiL);ML_1=ML_1*(-1)^(-1);
-[ML_1;ML0;ML1];
-Mnew=D(1,1)*ML_1+D(2,1)*ML0+D(3,1)*ML1;[MP1;Mnew];
-R = ott13.z_rotation_matrix(thetaL,phiL);
-Mnew=Mnew*R;
-[MP1;Mnew]
-%Ey has a -, why?
-
 %% our own code
 clc;
 n=1;m=1;Nmax=1;
@@ -41,7 +15,7 @@ ML1 = spharm(n,1,thetaL,phiL)*(-1);
 ML0 = spharm(n,0,thetaL,phiL);
 ML_1 = spharm(n,-1,thetaL,phiL)*(-1)^(-1);
 [ML_1;ML0;ML1];
-Mnew=D(1,1)*ML_1+D(2,1)*ML0+D(3,1)*ML1;[MP1;Mnew]
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew]
 %test Eq(35) of P312 of zeng's book:OK.
 EqL=D(:,2);
 EqR=sqrt(4*pi/3)*[ML_1,ML0,ML1]';
@@ -52,18 +26,12 @@ EqR=sqrt(4*pi/3)*[ML_1,ML0,ML1]';
 [ML0,NL0] = vswfcart(n,0,krL,thetaL,phiL);
 [ML_1,NL_1] = vswfcart(n,-1,krL,thetaL,phiL);ML_1=ML_1*(-1)^(-1);
 [ML_1;ML0;ML1];
-Mnew=D(1,1)*ML_1+D(2,1)*ML0+D(3,1)*ML1;[MP1;Mnew];
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew];
 R = ott13.z_rotation_matrix(thetaL,phiL);
 Mnew=Mnew*R;
 [MP1;Mnew]
-%Ey term has a -, why? may be the D is not the D in Mish, to test.
 
-%% Now the field
-[X,Y]=meshgrid([-1,0,1]);
-mt=(-1).^(Y-X);
-D1=D./mt
-
-%% test Ylm with the D from Mish:
+%% test Ylm with the D from Mish: OK
 clc;
 n=1;m=1;Nmax=1;
 krL=0.5; thetaL=pi/3;phiL=pi/6;
@@ -72,11 +40,11 @@ R = ott13.z_rotation_matrix(thetaL,0);
 d = ott13.wigner_rotation_matrix(Nmax,R);d=full(d.')
 %test Eq(B.28) of Mish on P338;
 EqL=d(:,2);
-EqR=legendre(1,cos(thetaL));ML_1=-0.5*EqR(2);EqR=[ML_1;EqR]
+EqR=legendre(1,cos(thetaL));ML_1=-0.5*EqR(2);EqR=[ML_1;EqR];
 mtmp=[-1;0;1];EqR=sqrt(factorial(n-mtmp)./factorial(n+mtmp)).*EqR;
-[EqL';EqR']'
+[EqL,EqR]
 alpha=phiL;% add m'*alpha to get D
-Dmish=[D1(1,:)*exp(-1i*(-1)*alpha);D1(2,:)*exp(-1i*0*alpha);D1(3,:)*exp(-1i*1*alpha)];
+Dmish=[d(1,:)*exp(-1i*(-1)*alpha);d(2,:)*exp(-1i*0*alpha);d(3,:)*exp(-1i*1*alpha)];
 D=full(Dmish)
 %test Eq(C.10)(sphere harmoncial function Ylm):OK
 MP1 = spharm(n,m,thetaP,phiP)*(-1);
@@ -84,20 +52,72 @@ ML1 = spharm(n,1,thetaL,phiL)*(-1);
 ML0 = spharm(n,0,thetaL,phiL);
 ML_1 = spharm(n,-1,thetaL,phiL)*(-1)^(-1);
 [ML_1;ML0;ML1];
-Mnew=D(1,1)*ML_1+D(2,1)*ML0+D(3,1)*ML1;[MP1;Mnew]
-%test Eq(35) of P312 of zeng's book:OK.
-EqL=D(:,2);
-EqR=sqrt(4*pi/3)*[ML_1,ML0,ML1]';
-[EqL';EqR']'
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew]
 %test Vector Mmn
 [MP1,NP1] = vswfcart(n,m,krP,thetaP,phiP);MP1=MP1*(-1);
 [ML1,NL1] = vswfcart(n,1,krL,thetaL,phiL);ML1=ML1*(-1);
 [ML0,NL0] = vswfcart(n,0,krL,thetaL,phiL);
 [ML_1,NL_1] = vswfcart(n,-1,krL,thetaL,phiL);ML_1=ML_1*(-1)^(-1);
 [ML_1;ML0;ML1];
-Mnew=D(1,1)*ML_1+D(2,1)*ML0+D(3,1)*ML1;[MP1;Mnew];
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew];
 R = ott13.z_rotation_matrix(thetaL,phiL);
 Mnew=Mnew*R;
 [MP1;Mnew]
-%Ey term has a -, why? may be the D is not the D in Mish, to test.
 
+%% test Ylm and Mlm with the D from OTT:OK
+% clc;
+n=1;m=1;Nmax=1;
+krL=0.5; thetaL=pi/3;phiL=pi/6;
+krP=0.5; thetaP=0;phiP=0;
+R = ott13.z_rotation_matrix(thetaL,phiL);
+D = ott13.wigner_rotation_matrix(Nmax,R);D=full(D.')% This is the Zeng's D, m={-1,0,1} order.
+%test Ylm
+MP1 = spharm(n,m,thetaP,phiP)*(-1);MP1;
+ML1 = spharm(n,1,thetaL,phiL)*(-1);
+ML0 = spharm(n,0,thetaL,phiL);
+ML_1 = spharm(n,-1,thetaL,phiL)*(-1)^(-1);
+[ML_1;ML0;ML1]; %verified by mathecmatica
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew]
+%test vector M
+[MP1,NP1] = vswfcart(n,m,krP,thetaP,phiP);MP1=MP1*(-1);
+[ML1,NL1] = vswfcart(n,1,krL,thetaL,phiL);ML1=ML1*(-1);
+[ML0,NL0] = vswfcart(n,0,krL,thetaL,phiL);
+[ML_1,NL_1] = vswfcart(n,-1,krL,thetaL,phiL);ML_1=ML_1*(-1)^(-1);
+[ML_1;ML0;ML1];
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew];
+R = ott13.z_rotation_matrix(thetaL,phiL);
+Mnew=Mnew*R;
+[MP1;Mnew]
+%% Now the field
+[X,Y]=meshgrid([-1,0,1]);
+mt=(-1).^(Y-X);
+D1=D./mt
+
+%% test Ylm and Mlm with the D from OTT:OK
+% clc;
+n=1;m=1;Nmax=1;
+krL=0.5; thetaL=pi/3;phiL=pi/6;
+krP=0.5; thetaP=0;phiP=0;
+R = ott13.z_rotation_matrix(thetaL,phiL);
+D = ott13.wigner_rotation_matrix(Nmax,R);D=full(D.');
+[X,Y]=meshgrid([-1,0,1]);
+mt=(-1).^(Y-X);
+D=mt.*D;
+%D=full(D.')% This is the Zeng's D, m={-1,0,1} order.
+%test Ylm
+MP1 = spharm(n,m,thetaP,phiP);%*(-1);MP1;
+ML1 = spharm(n,1,thetaL,phiL);
+ML0 = spharm(n,0,thetaL,phiL);
+ML_1 = spharm(n,-1,thetaL,phiL);
+[ML_1;ML0;ML1]; %verified by mathecmatica
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew]
+%test vector M
+[MP1,NP1] = vswfcart(n,m,krP,thetaP,phiP);%MP1=MP1*(-1);
+[ML1,NL1] = vswfcart(n,1,krL,thetaL,phiL);%ML1=ML1*(-1);
+[ML0,NL0] = vswfcart(n,0,krL,thetaL,phiL);
+[ML_1,NL_1] = vswfcart(n,-1,krL,thetaL,phiL);%ML_1=ML_1*(-1)^(-1);
+[ML_1;ML0;ML1];
+Mnew=D(1,3)*ML_1+D(2,3)*ML0+D(3,3)*ML1;[MP1;Mnew];
+R = ott13.z_rotation_matrix(thetaL,phiL);
+Mnew=Mnew*R;
+[MP1;Mnew]
