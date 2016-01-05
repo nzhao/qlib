1f7d4f76eab3f9f7293027fc4b3736948d25adb4
nzhao <nzhao@csrc.ac.cn>
2016-01-05 16:19:46 +0800

    87Rb atomic structure

---
+++ temp/Rb87.m
@@ -0,0 +1,31 @@
+clear; clc
+import model.phy.PhysicalObject.Atom.AlkaliAtom
+
+rb=AlkaliAtom('87Rb');
+
+magB=0.0000;
+gsHami=rb.gsHamiltonian(magB);
+[gsV, gsD]=eig(gsHami);
+
+gsValue=diag(gsD);
+zeroField_gsValue=[gsValue(1), gsValue(end)];
+h0coupled_representation=gsV'*gsHami*gsV;
+
+magB1=0.0001;
+gsHami1=rb.gsHamiltonian(magB1);
+[gsV1, gsD1]=eig(gsHami1);
+h1coupled_representation=gsV'*gsHami1*gsV;
+idx=[1 4 2 5 3 7 6 8];
+reOrder_h1=h1coupled_representation(idx,idx);
+
+gsValue1=diag(gsD1);
+diffF1=diff(gsValue1(1:3))/2/pi;
+diffF2=diff(gsValue1(4:end))/2/pi;
+
+
+blist=0:0.0010:1.0000;
+vlist=zeros(8, length(blist));
+for i=1:length(blist)
+    vlist(:,i)=eig(rb.gsHamiltonian(blist(i)));
+end
+plot(blist, vlist')
