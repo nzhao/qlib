f0e116653e1383536ec70aef98af9cc6d1e4b737
zhangke2821 <zkwzk@csrc.ac.cn>
2016-01-08 10:26:36 +0800

    add
    add some  progams

--- temp/csAtom.m
+++ temp/csAtom.m
@@ -1,17 +1,10 @@
 clear; clc
 import model.phy.PhysicalObject.Atom.AlkaliAtom
 
-rb=AlkaliAtom('87Rb');
-magB=0.0100; j=1.5;
-esHami=rb.esHamiltonian(j, magB);
-thetaD=10.; phiD=8.0; eTheta=1.0; ePhi=0.0; Sl=1.0;
-kVec=[thetaD, phiD]; pol=[eTheta, ePhi];
-rb.setBeam( 'D1', kVec, pol, Sl)
-
-tV=rb.electricDipoleInteraction(j, magB);
+cs=AlkaliAtom('133Cs');
 
 magB=0.0000;
-gsHami=rb.gsHamiltonian(magB);
+gsHami=cs.gsHamiltonian(magB);
 [gsV, gsD]=eig(gsHami);
 
 gsValue=diag(gsD);
@@ -19,7 +12,7 @@ zeroField_gsValue=[gsValue(1), gsValue(end)];
 h0coupled_representation=gsV'*gsHami*gsV;
 
 magB1=0.0001;
-gsHami1=rb.gsHamiltonian(magB1);
+gsHami1=cs.gsHamiltonian(magB1);
 [gsV1, gsD1]=eig(gsHami1);
 h1coupled_representation=gsV1'*gsHami1*gsV1;
 idx=[1 4 2 5 3 7 6 8];
@@ -31,9 +24,9 @@ diffF2=diff(gsValue1(4:end))/2/pi;
 
 
 blist=0:0.0010:1.0000;
-vlist=zeros(8, length(blist));
+vlist=zeros(16, length(blist));
 for i=1:length(blist)
-    vlist(:,i)=eig(rb.gsHamiltonian(blist(i)));
+    vlist(:,i)=eig(cs.gsHamiltonian(blist(i)));
 end
 plot(blist, vlist')
 
